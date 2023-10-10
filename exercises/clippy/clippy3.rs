bbbfd529ef91e6ff703819ad15436b745ea2e94e
schwarz <root.schwarz.chan@gmail.com>
2023-10-11 01:12:44 +0800

    update

--- exercises/clippy/clippy3.rs
+++ exercises/clippy/clippy3.rs
@@ -8,7 +8,7 @@
 fn main() {
     let my_option: Option<()> = None;
     if my_option.is_none() {
-        println!("{}", my_option);
+        println!("{:?}", my_option);
     }
 
     let my_arr = &[-1, -2, -3, -4, -5, -6];
